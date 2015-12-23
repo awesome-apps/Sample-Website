c2d26a987bd005893615c541f3f7a4e6920d4cfc
awesome-apps <scideastech6@gmail.com>
2015-12-22 19:50:52 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 document.body.style.backgroundColor = "rgb(0, 115, 255)";
+var tag = document.getElementsByTagName("p")[0];
+text = tag.innerHTML;
+// Here I would like to call the Python interpreter with Python function
+arrOfStrings = openSomehowPythonInterpreter("~/client.py", "stuff()");
+
+~/pythoncode.py
