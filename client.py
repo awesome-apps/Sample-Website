fbda2c5ffbd10ea99fa06e71361c41b2241e71e0
awesome-apps <scideastech6@gmail.com>
2015-12-22 19:51:23 -0800

    Create client.py

---
+++ client.py
@@ -0,0 +1,23 @@
+def stuff():
+        
+    import os
+    import socket
+    import time
+
+    s = socket.socket()
+
+    while True:
+        try:
+            s.connect(("localhost", 44000))
+            break
+        except:
+            time.sleep(1)
+
+    messages = [os.name, "quit"]
+    x = 0
+    while x <= len(messages):
+        s.send(messages[x])
+        x += 1
+    
+    s.close()
+
